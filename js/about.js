2b26138c844b2219a8cc6d33ef63cdd925d1408c
rajendra905 <65326732+rajendra905@users.noreply.github.com>
2020-08-30 13:46:17 +0530

    about

--- js/about.js
+++ js/about.js
@@ -1,30 +1,19 @@
 jQuery(document).ready(function($) {
 
-    $('.owl-carousel-blog').owlCarousel({
-        center: false,
-        items: 1,
-        loop: true,
-        stagePadding: 30,
-        margin: 30,
-        autoplay: false,
-        pauseOnHover: false,
-        nav: true,
-        dots:false,
-        smartSpeed:1000,
-        navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">'],
-        responsive:{
-            0:{
-                items:1,
-                nav:true
-            },
-            600:{
-                items:1,
-                nav:true
-            },
-            1000:{
-                items:1,
-                nav:true,
-            }
+   $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
         }
-      });
+    }
+})
 });
